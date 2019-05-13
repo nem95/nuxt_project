47b6a75bb5112e8e4f9f4175b9c0d340bcf0130e
Naïm <naiim.chatti@gmail.com>
2019-05-13 15:29:21 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+  env: {
+    baseUrl: "https://my-json-server.typicode.com/alexandre-tobia/api-sample"
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' },
+    ],
+  },
+  generate: {
+    routes: [
+      '/blog/1',
+      '/blog/2',
+      '/blog/3',
+    ],
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/blog/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="currentArticle">
+    <nuxt-link :to="{ name: 'blog'}">
+      <button type="button" class="btn btn-primary">Retour</button>
+    </nuxt-link>
+    Article numéro {{ $route.params.id }}
+    <br>
+    Article title : {{ currentArticle.title }}
+  </div>
+</template>
+
+<script>
+  import axios from '~/plugins/axios';
+
+  export default {
+    async asyncData({ params }) {
+      console.log(params);
+
+      const { data } = await axios.get(`/servers/${params.id}`);
+      console.log(data);
+    },
+    data() {
+      return {
+        currentArticle: null,
+        articles: [
+          {
+            id: 1,
+            title: "Article 1",
+            description: "Article numéro 1",
+          },
+          {
+            id: 2,
+            title: "Article 2",
+            description: "Article numéro 2",
+          },
+          {
+            id: 3,
+            title: "Article 3",
+            description: "Article numéro 3",
+          },
+        ]
+      }
+    },
+    mounted() {
+      this.currentArticle = this.articles.find(article => article.id === parseInt(this.$route.params.id));
+    },
+    head() {
+      return {
+        title: "Article",
+      }
+    },
+  }
+</script>
